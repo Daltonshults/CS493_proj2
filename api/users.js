5f970f9e46e52a9f25a2b08f6c1174a7f5618a33
Daltonshults <Dshults@cocc.edu>
2024-05-06 18:27:11 -0700

    Removed console.log statements, and adjusted the results to include photos and reviews.

--- api/users.js
+++ api/users.js
@@ -1,30 +1,26 @@
 const router = require('express').Router();
-// const util = require('util');
-// const mysql = require('mysql2/promise');
 const { getPool } = require('../lib/mysql_connection');
 exports.router = router;
 
-const { businesses } = require('./businesses');
-const { reviews } = require('./reviews');
-const { photos } = require('./photos');
 
 router.get('/:userid/businesses', async function (req, res) {
   const userid = parseInt(req.params.userid);
-  // console.log(userid);
-
   try {
-    let query = `SELECT * FROM Businesses WHERE OwnerID = 1`;
-
-    const results = await getPool().query(
+    const [results] = await getPool().query(
       `SELECT * FROM Businesses WHERE OwnerID = ?`, [userid]
     );
 
-    // console.log(results[0]);
+    for (let i = 0; i < results.length; i++) {
+      const business = results[i];
+      const [reviews] = await getPool().query('SELECT * FROM Reviews WHERE BusinessID = ?', [business.BusinessID]);
+      business.reviews = reviews;
 
-
-    if (results[0].length > 0) {
+      const [photos] = await getPool().query('SELECT * FROM Photos WHERE BusinessID = ?', [business.BusinessID]);
+      business.photos = photos;
+    }
+    if (results.length > 0) {
       res.status(200).json({
-        businesses: results[0]
+        businesses: results
       });
     } else {
       res.status(404).json({
@@ -45,19 +41,13 @@ router.get('/:userid/reviews', async function (req, res) {
   const userid = parseInt(req.params.userid);
 
   try {
-    const results = await getPool().query(
+    const [results] = await getPool().query(
       `SELECT * FROM Reviews WHERE UserID = ?`, [userid]
     );
 
-    // console.log("Results");
-    // console.log(results);
-
-    // console.log("Results[0]");
-    // console.log(results[0]);
-
-    if (results[0].length > 0 ) {
+    if (results.length > 0 ) {
       res.status(200).json({
-        reviews: results[0]
+        reviews: results
       });
     } else {
       res.status(404).json({
@@ -81,12 +71,6 @@ router.get('/:userid/photos', async function (req, res) {
     const results = await getPool().query(
       `SELECT * FROM Photos WHERE UserID = ?`, [userid]
     );
-    // console.log(`resultslength: ${results.length}`);
-    // console.log("Results");
-    // console.log(results);
-
-    // console.log("Results[0]");
-    // console.log(results[0]);
 
     if (results[0].length > 0) {
       res.status(200).json({
